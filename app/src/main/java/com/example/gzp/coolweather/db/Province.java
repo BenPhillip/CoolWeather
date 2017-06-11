69999bd74078e334cd6ef88211ad42ce72af6ded
BenPhillip <p627121110@163.com>
2017-06-11 14:43:43 +0800

    创建项目并创建了数据库和表的各项配置

---
+++ app/src/main/java/com/example/gzp/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.gzp.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Ben on 2017/6/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
